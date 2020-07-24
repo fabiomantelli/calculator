0358550b09b28ae8547d09f74511e88148a7c4d9
Fabio Mantelli <fmmantelli@gmail.com>
2020-07-24 17:44:31 -0300

    refactor: changed router variable to calculatorRouter variable name

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = require('express')()
-const router = require('./src/routers')
+const index = require('./src/routers')
+const calculatorRouter = require('./src/routers/calculator.route')
 
-app.use('/calculator', router)
+app.use(index)
+app.use('/calculator', calculatorRouter)
 app.listen(8080)
 
 exports.app = app
